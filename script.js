ba8e359467e4d46a089f534a31f0c0e79baa1b50
Orcutt <Jimmy.Orcutt@energyTransfer.com>
2017-07-11 16:59:03 -0500

    shakin envelope

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-    $('div').effect('slide',{direction:'down'},1000)
+    $('div').effect('slide',{direction:'down'},1000,function(){
+        $('i').effect("bounce",{times:1},450);
+    });
     $('p').draggable();
     $('h2').draggable();
     $('h1').draggable();
